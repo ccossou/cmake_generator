a90b8dd9c89940a9bc80d8d6321ea8167c8eab3f
Christophe COSSOU <christophe.cossou@cea.fr>
2020-12-15 11:54:01 +0100

    First commit, init of package

---
+++ cmake_generator/__init__.py
@@ -0,0 +1,6 @@
+import logging
+logging.getLogger("cmake_generator").addHandler(logging.NullHandler())
+
+from .version import __version__
+
+from . import utils
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Setup file
+"""
+
+import io
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="cmake_generator",
+    version=find_version("cmake_generator", "version.py"),
+    description="",
+    url="",
+    author="",
+    author_email="",
+    packages=find_packages(),
+    package_data={},
+    scripts=[],
+    data_files=[('', ['README.adoc'])]
+)
